e0c179f5fed7baf0cb109d22c6f839672a0b46f3
erfan <erfangkl1@gmail.com>
2025-01-15 15:32:28 +0100

    not happend to this commit

---
+++ src/services/servises.js
@@ -0,0 +1,73 @@
+
+
+import App from "../App"
+import { useEffect, useState } from "react"
+
+
+
+
+function Servises() {
+
+
+    return (
+
+
+        <div>
+            <hr className="line" />
+            <p className="content1">servise</p>
+            <h1 className="content1">Check out our services</h1>
+
+
+            <div className="servise">
+                <h1 className="">servise</h1>
+                <p className="">Please keep me updated by email with the latest crypto news, research findings, reward programs, event updates, coin listings and more information from CoinMarketCap.
+                </p>
+
+            </div>
+            <div className="servise">
+                <h1 className="">servise</h1>
+                <p className="">Please keep me updated by email with the latest crypto news, research findings, reward programs, event updates, coin listings and more information from CoinMarketCap.
+                </p>
+
+            </div>
+            <div className="servise">
+                <h1 className="">servise</h1>
+                <p className=""> Please keep me updated by email with the latest crypto news, research findings, reward programs, event updates, coin listings and more information from CoinMarketCap.
+                </p>
+
+            </div>
+            <div className="servise">
+                <h1 className=""> servise</h1>
+                <p className="">Please keep me updated by email with the latest crypto news, research findings, reward programs, event updates, coin listings and more information from CoinMarketCap.
+                </p>
+
+            </div>
+            <div className="servise">
+                <h1 className="">servise</h1>
+                <p className="">Please keep me updated by email with the latest crypto news, research findings, reward programs, event updates, coin listings and more information from CoinMarketCap.
+                </p>
+
+            </div>
+            <div className="servise">
+                <h1 className="">servise</h1>
+                <p className="">Please keep me updated by email with the latest crypto news, research findings, reward programs, event updates, coin listings and more information from CoinMarketCap.
+                </p>
+
+            </div>
+
+
+
+
+        </div>
+
+
+    )
+
+
+
+
+}
+
+
+
+export default Servises
